435ebe19c69b98bc5d7e4953671cd082fffc69a7
Lasercar <64717068+Lasercar@users.noreply.github.com>
2025-03-11 20:26:40 +1000

    Restart from results state
    tallieslevel and conductor reset

--- source/funkin/play/ResultState.hx
+++ source/funkin/play/ResultState.hx
@@ -807,6 +807,17 @@ class ResultState extends MusicBeatSubState
       }
     }
 
+    if (controls.RESET)
+    {
+      if (PlayState.instance == null) return; // Do nothing - there's no playstate to return to
+      FlxTimer.globalManager.clear();
+      FlxTween.globalManager.clear();
+      if (introMusicAudio != null) introMusicAudio.stop();
+      // if (resultsMusic != null) resultsMusic.stop();
+      this.close();
+      return;
+    }
+
     if (controls.PAUSE_P || controls.ACCEPT_P #if mobile || TouchUtil.pressAction() #end)
     {
       if (busy) return;
